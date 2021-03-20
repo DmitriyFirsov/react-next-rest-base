2677b191b40c18a18dcba8d4d96934ba20120b87
ghost-str <email@example.com>
2021-03-20 15:15:18 +0300

    fix

---
+++ config/theme.ts
@@ -0,0 +1,30 @@
+import { DefaultTheme } from 'styled-components';
+
+export enum Breakpoints {
+  xxs = 0,
+  xs = 360,
+  sm = 512,
+  md = 768,
+  lg = 1024,
+  xl = 1200,
+  xxl = 1600,
+}
+
+export enum Colors {
+  link = '#00f',
+}
+
+export const up = (breakpoint: Breakpoints, vertical = false) => `@media (min-${vertical ? 'height' : 'width'}: calc(${breakpoint} + 0.02px))`;
+export const down = (breakpoint: Breakpoints, vertical = false) => `@media (max-${vertical ? 'height' : 'width'}: ${breakpoint})`;
+export const between = (breakpointMin: Breakpoints, breakpointMax: Breakpoints, vertical = false) =>
+  `@media (max-${vertical ? 'height' : 'width'}: ${breakpointMax}) and (min-${vertical ? 'height' : 'width'}: calc(${breakpointMin} + 0.02px))`;
+
+const theme: DefaultTheme = {
+  breakpoints: Breakpoints,
+  colors: Colors,
+  up,
+  down,
+  between,
+};
+
+export default theme;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import React, { FC } from 'react';
 import { AppProps } from 'next/app';
 import { wrapper } from '../redux/store';
+import theme from '../config/theme';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../config/globalStyles';
 
 const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <Component {...pageProps} />
+  </ThemeProvider>
 );
 
 export default wrapper.withRedux(WrappedApp);
